3ff9e867f78e701697b3ece3a9dd19d23cb768b3
Steve-Dusty <kuantingk2@gmail.com>
2020-08-11 20:46:28 -0700

    THis alien invasion is bugged

---
+++ shipthatfiresbullet.py/settings.py
@@ -0,0 +1,14 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        #screen settings
+
+        self.screen_width = 700
+        self.screen_height = 700
+        self.bg_color = (230, 230, 230)
+
+        #settings.py
+        self.ship_speedd = 1.5
+        
